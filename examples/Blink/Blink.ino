84bf25615fbc53ea602e6e551ac60338c66488a6
jumejume1 <jumejume1@hotmail.com>
2019-07-14 16:06:01 +0700

    Add files via upload

---
+++ examples/Blink/Blink.ino
@@ -0,0 +1,23 @@
+/*
+ * Example of a simple Blink using the AX-12A built-in LED
+ */
+
+#include "Arduino.h"
+#include "../../src/AX12A.h"
+
+#define DirectionPin 	(10u)
+#define BaudRate  		(1000000ul)
+#define ID				(1u)
+
+void setup()
+{
+	ax12a.begin(BaudRate, DirectionPin, &Serial);
+}
+
+void loop()
+{
+	ax12a.ledStatus(ID, ON);
+	delay(1000);
+	ax12a.ledStatus(ID, OFF);
+	delay(1000);
+}
